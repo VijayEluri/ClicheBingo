414c74cc627c41322803ba32c32401fe076e089e
Tomas Remotigue <tomremo@gmail.com>
2012-08-04 00:02:24 -0700

    Initial application stub

---
+++ src/com/rogueitem/clichebingo/app/ClicheBingoApp.java
@@ -0,0 +1,13 @@
+package com.rogueitem.clichebingo.app;
+
+public class ClicheBingoApp {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Hello world");
+  }
+
+}
